c046b110585bdde2b1534f85aabf4eef4dac6f4e
dc7290 <dhkh.cba0927@gmail.com>
2022-06-17 18:41:44 +0900

    不要な記述を削除

--- astro-microcms-blog/astro.config.mjs
+++ astro-microcms-blog/astro.config.mjs
@@ -1,10 +1,11 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
+import { defineConfig } from 'astro/config'
+import preact from '@astrojs/preact'
 
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+import robotsTxt from 'astro-robots-txt'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind(), sitemap()]
-});
+  integrations: [preact(), tailwind(), sitemap(), robotsTxt()],
+})
